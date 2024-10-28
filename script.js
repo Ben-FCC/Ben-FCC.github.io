7533e254e940e2d67aad76fa6024611e48b483c3
Feng-Chun <65886339+Feng-Chun@users.noreply.github.com>
2024-10-28 21:38:34 +0800

    init homepage

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    new Typed('.typed-text', {
+        strings: [
+            "PhD Student @ NTU",
+            "Interests: Computational Social Science and Interacting Minds",
+        ],
+        typeSpeed: 80,
+        backSpeed: 50,
+        backDelay: 1500,
+        loop: true
+    });
+});
